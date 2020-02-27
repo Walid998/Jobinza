e59a0f64e7fc2c2675bfbe05c387c4dfb3711f8c
walaa <walaa>
2020-02-27 18:51:35 +0200

    Date $ status

--- jobinza/company/views.py
+++ jobinza/company/views.py
@@ -10,6 +10,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Q
 from django.http import HttpResponse
+from django.utils import timezone
+import datetime
 
 from company.models import CreatePost
 from company.forms import CreatePostForm
@@ -27,14 +29,26 @@ def create_post_view(request):
 		author = Account.objects.filter(email=user.email).first()
 		obj.author = author
 		obj.save()
+		#return render(request, "company/create_post.html")
 		form = CreatePostForm()
 
 	context['form'] = form
-	return render(request, "company/create_post.html", context)
+	#return render(request, "company/list_job.html")
+	return render(request, "company/create_post.html" , context)
+
+
+def update_status():
+	now = datetime.date.today()
+	form = CreatePost.objects.all()
+	for doc in form :
+		if doc.deadline < now or doc.deadline == now:
+			doc.status = 'closed'
+		doc.save()
 
 
 @login_required(login_url='login')
 def list_job_view(request):
+	update_status()
 	listpost = CreatePost.objects.all()
 	context = {
 		'title' : 'list jobs',
