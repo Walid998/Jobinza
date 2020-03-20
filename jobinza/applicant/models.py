c7a3efce64c516b8ebdb00ced08b2bf0a7ebcd48
NOUR <NOUR@DESKTOP-SFUDGUF>
2020-03-20 21:23:08 +0200

    edit

--- jobinza/applicant/models.py
+++ jobinza/applicant/models.py
@@ -6,7 +6,7 @@ from company.models import CreatePost
 
 
 class Profile(models.Model):
-    #image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default="{% static 'jobinza/images/profile-applicant.jpg' %}", upload_to="{% static 'jobinza/images/profile-pics/ %}")
     #user = models.OneToOneField(User, on_delete=models.CASCADE)
     phonenumber = models.CharField(max_length=50, null=False, blank=True)
     address = models.CharField(max_length=50, null=False, blank=True)
