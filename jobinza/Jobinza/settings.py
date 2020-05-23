cd8903874a89ffbb975fc12aa5372ff89ec58204
unknown <walaamofata398@gmail.com>
2020-05-23 17:21:19 +0200

    filtration ui

--- jobinza/Jobinza/settings.py
+++ jobinza/Jobinza/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django',
+#    'social_django',
     'crispy_forms',
     'company.apps.CompanyConfig',
     'account.apps.AccountConfig',
